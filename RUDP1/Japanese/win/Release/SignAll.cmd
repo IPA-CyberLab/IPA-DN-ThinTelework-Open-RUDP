d00ff6fb8654cb56a8e395dcf783ffac70de8f78
Daiyuu Nobori <daiyuu.git@softether.co.jp>
2020-11-09 15:55:22 +0900

    build OK!

--- RUDP1/Japanese/win/Release/SignAll.cmd
+++ RUDP1/Japanese/win/Release/SignAll.cmd
@@ -4,14 +4,14 @@ rem -- ÇÊÇ≠Çºå©Ç¬ÇØÇ»Ç≥Ç¡ÇΩ! --
 
 rem -- å©ÇÈÇ»ÅIÅI --
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\logmessages.dll
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile logmessages.dll
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\urdpclient.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile urdpclient.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\urdpconfig.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile urdpconfig.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\urdpserver.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile urdpserver.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\git\IPA-DN-ThinTelework-RUDP\RUDP1\Japanese\win\Release\wm_hooks.dll
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile wm_hooks.dll
 
 
--- RUDP2/ReleaseNoUnicode/SignAll.cmd
+++ RUDP2/ReleaseNoUnicode/SignAll.cmd
@@ -4,15 +4,15 @@ rem -- ÇÊÇ≠Çºå©Ç¬ÇØÇ»Ç≥Ç¡ÇΩ! --
 
 rem -- å©ÇÈÇ»ÅIÅI --
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\Git\IPA-DN-ThinTelework-RUDP\RUDP2\ReleaseNoUnicode\hookldr.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile hookldr.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\Git\IPA-DN-ThinTelework-RUDP\RUDP2\ReleaseNoUnicode\urdpclient2.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile urdpclient2.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\Git\IPA-DN-ThinTelework-RUDP\RUDP2\ReleaseNoUnicode\urdpserver2.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile urdpserver2.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\Git\IPA-DN-ThinTelework-RUDP\RUDP2\ReleaseNoUnicode\viewer-keysym-test.exe
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile viewer-keysym-test.exe
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\Git\IPA-DN-ThinTelework-RUDP\RUDP2\ReleaseNoUnicode\screenhooks32.dll
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile screenhooks32.dll
 
-S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile C:\Git\IPA-DN-ThinTelework-RUDP\RUDP2\ReleaseNoUnicode\setup-helper.dll
+S:\Desk\ExeSignTool\BuildUtil.exe /cmd:SignCode2 /COMMENT:"User Mode Remote Desktop" /KERNEL:no /CERT:DaiyuuNoboriFile setup-helper.dll
 
